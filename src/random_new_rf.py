71d4c21486adae019a4b07eb9908bc190c7b9266
DinhQuan <quanhero2k1@gmail.com>
2024-04-11 13:56:03 +0900

    Refactor code for Robotic Module

--- src/random_new_rf.py
+++ src/random_new_rf.py
@@ -3,10 +3,24 @@ from robodk.robomath import *  # basic matrix operations
 import numpy as np
 import time
 import create_ref as cr
+import sys
+sys.path.append('../')
+import config as CFG
+
 
 # import keyboard
 import rot_pos as rp
+import vision as vis#temp
 
+def getCoordinates(flag: bool):
+    obj = vis.VisionModule()
+    obj._start_()
+    obj._getImage_()
+    obj.load_model()
+    coordinate = obj._getCoordinate_()
+    obj.save_image()
+    obj._end_()
+    return coordinate
 
 def drawPolygon(target_rf, n_sides, R):
 
@@ -60,8 +74,8 @@ def changeJoint(rf_camera2base, rf_surface2base):
 
 def convertCoordinates(focal_length, res_height, res_width, pixel_x, pixel_y, distance):
 
-    sensor_height = res_height * 3.45 / 1000
-    sensor_width = res_width * 3.45 / 1000
+    sensor_height = res_height * CFG.PIXEL_SIZE / 1000 #cfg
+    sensor_width = res_width * CFG.PIXEL_SIZE / 1000 #cfg
     sensor_size = np.array([sensor_height, sensor_width])
     print("sensor_size:", sensor_size, "\n")
 
@@ -123,7 +137,7 @@ def createPoint(x_st, y_st, z_st):
     # print("pos_target:", all_pos_target, "\n")
     # print("all_target_matrix:", len(all_target_matrix), "\n")
 
-    speeds = [100, 50]
+    speeds = CFG.SPEEDS #cfg - TEST
     count = 0
     robot.setSpeed(speeds[1])
     robot.MoveL(target_matrix)
@@ -204,29 +218,29 @@ theta6 = np.radians(90)
 """
 RDK = Robolink()
 
-robot = RDK.AddFile("E:\\Install-software\\RoboDK\\Library\\Motoman-GP8.robot")
+# robot = RDK.AddFile("E:\\Install-software\\RoboDK\\Library\\Motoman-GP8.robot")
 robot = RDK.ItemUserPick("Yaskawa GP8 Base", ITEM_TYPE_ROBOT)
 if not robot.Valid():
     raise Exception("No robot selected or available")
 
 
-# RUN_ON_ROBOT = True
+RUN_ON_ROBOT = True
 
-# if RDK.RunMode() != RUNMODE_SIMULATE:
-#     RUN_ON_ROBOT = False
+if RDK.RunMode() != RUNMODE_SIMULATE:
+    RUN_ON_ROBOT = False
 
-# if RUN_ON_ROBOT:
-#     # Connect to the robot using default IP
-#     success = robot.Connect()  # Try to connect once
-#     #success robot.ConnectSafe() # Try to connect multiple times
-#     status, status_msg = robot.ConnectedState()
-#     if status != ROBOTCOM_READY:
-#         # Stop if the connection did not succeed
-#         print(status_msg)
-#         raise Exception("Failed to connect: " + status_msg)
+if RUN_ON_ROBOT:
+    # Connect to the robot using default IP
+    success = robot.Connect()  # Try to connect once
+    #success robot.ConnectSafe() # Try to connect multiple times
+    status, status_msg = robot.ConnectedState()
+    if status != ROBOTCOM_READY:
+        # Stop if the connection did not succeed
+        print(status_msg)
+        raise Exception("Failed to connect: " + status_msg)
 
-#     # This will set to run the API programs on the robot and the simulator (online programming)
-#     RDK.setRunMode(RUNMODE_RUN_ROBOT)
+    # This will set to run the API programs on the robot and the simulator (online programming)
+    RDK.setRunMode(RUNMODE_RUN_ROBOT)
 
 robot.ConnectedState()
 print("ConnectedState:", robot.ConnectedState(), "\n")
@@ -269,7 +283,12 @@ rf_surface2camera = cr.createRef(pos_surface2camera, rot_surface2camera)
 print("rf_surface2camera:", rf_surface2camera, "\n")
 
 # reference frame image to surface
-x_target, y_target, h, w = convertCoordinates(16, 2048, 2048, 750, 1280, 480)
+
+coordinate_pixel = getCoordinates(True)
+
+x_target, y_target, h, w = convertCoordinates(CFG.FOCAL_LENGTH, CFG.RESOLUTION_X, CFG.RESOLUTION_Y, coordinate_pixel[-1],\
+                                                                                                    coordinate_pixel[0], CFG.DISTANCE_2OBJECT) #cfg
+
 pos_img2sur = [int(w / 2), int(h / 2), 0]
 rot_img2sur = [0, 0, 0]
 
@@ -304,10 +323,14 @@ sleep_seconds(10)
 # print("target_matrix:", target_matrix, "\n")
 
 createPoint(x_target, y_target, 0)
+#TEST FLOW
+x_target_02, y_target_02, h, w = convertCoordinates(CFG.FOCAL_LENGTH, CFG.RESOLUTION_X, CFG.RESOLUTION_Y, list(coordinate_pixel)[-1] + 800,\
+                                                                                                    list(coordinate_pixel)[0], CFG.DISTANCE_2OBJECT)
+# createPoint(x_target_02, y_target_02, 0)
 
 # robot.MoveJ(fixed_target_matrix)
 
-# robot.MoveJ(rf_camera2base_matrix)
+robot.MoveJ(rf_camera2base_matrix)
 current_joint_values = robot.Joints()
 print("current_joint_values:", current_joint_values, "\n")
 
