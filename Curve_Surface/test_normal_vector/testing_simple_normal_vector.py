840d6d3dfc8d0b6f9fb3b31a622a4952c5f38aa7
nguyendinhquan140701 <quanhero2k1@gmail.com>
2024-05-16 11:26:05 +0900

    test PCA and simple normal vector

--- Curve_Surface/test_normal_vector/testing_simple_normal_vector.py
+++ Curve_Surface/test_normal_vector/testing_simple_normal_vector.py
@@ -5,6 +5,37 @@ from sklearn.neighbors import NearestNeighbors
 import open3d as o3d
 
 
+def pcd_rotation(point_cloud, roll_deg=0.0, pitch_deg=0.0, yaw_deg=0.0):
+    roll_T = np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(np.deg2rad(roll_deg)), -np.sin(np.deg2rad(roll_deg))],
+            [0, np.sin(np.deg2rad(roll_deg)), np.cos(np.deg2rad(roll_deg))],
+        ]
+    )
+
+    pitch_T = np.array(
+        [
+            [np.cos(np.deg2rad(pitch_deg)), 0, np.sin(np.deg2rad(pitch_deg))],
+            [0, 1, 0],
+            [-np.sin(np.deg2rad(pitch_deg)), 0, np.cos(np.deg2rad(pitch_deg))],
+        ]
+    )
+
+    yaw_T = np.array(
+        [
+            [np.cos(np.deg2rad(yaw_deg)), -np.sin(np.deg2rad(yaw_deg)), 0],
+            [np.sin(np.deg2rad(yaw_deg)), np.cos(np.deg2rad(yaw_deg)), 0],
+            [0, 0, 1],
+        ]
+    )
+
+    np_point_cloud = point_cloud.reshape((-1, 3))
+    t_pcd = np.matmul(np_point_cloud, np.matmul(np.matmul(yaw_T, pitch_T), roll_T))
+    print(np.matmul(np.matmul(yaw_T, pitch_T), roll_T))
+    return t_pcd
+
+
 def calculate_point_normalvector(p1, p2, p3, reference_point):
     # p1 기준점(reference point)
     # p1, p2, p3 data type [x, y, z]
@@ -29,7 +60,7 @@ def simple_normal_vector(source):
     near_sample_num = 3
 
     # 가까운 거리 search을 효율적으로하기 위하여
-    neigh = NearestNeighbors(n_neighbors=near_sample_num, algorithm="kd_tree", radius= 0.1)
+    neigh = NearestNeighbors(n_neighbors=near_sample_num)
     neigh.fit(source)
 
     normal_vector_list = []
@@ -43,6 +74,7 @@ def simple_normal_vector(source):
         indices = indices.ravel()
 
         p1, p2, p3 = source[indices]
+        print(f"p1: {p1}, p2: {p2}, p3: {p3}")
         nv = calculate_point_normalvector(p1, p2, p3, src_point)
 
         normal_vector_list.append(nv)
@@ -68,15 +100,25 @@ def pcd_show(point_clouds=[]):
 
 if __name__ == "__main__":
     coord = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    x = np.linspace(-1, 1, 10)
-    y = np.linspace(-1, 1, 10)
-    # x = np.random.uniform(1, 10, 10)
-    # y = np.random.uniform(1, 10, 10)
-    X, Y = np.meshgrid(x, y)
-    Z = -(1 / 3) * (X**2) + -(1 / 3) * Y**2  # Phương trình parabol
-    # Z = - (X**2) + - Y**2  # Phương trình parabol
-    target = np.stack([X.flatten(), Y.flatten(), Z.flatten()], axis=-1)
-    source = target.reshape((-1, 3))
+    # x = np.linspace(-1, 1, 10)
+    # y = np.linspace(-1, 1, 10)
+    # # x = np.random.uniform(1, 10, 10)
+    # # y = np.random.uniform(1, 10, 10)
+    # X, Y = np.meshgrid(x, y)
+    # Z = -(X**2) + - Y**2  # Phương trình parabol
+    # # Z = - (X**2) + - Y**2  # Phương trình parabol
+
+    theta_sample_num = 50
+    theta = np.linspace(0.0, 2 * np.pi, theta_sample_num)
+    r = 1.0
+    X = theta
+    Y = r * np.sin(theta)
+    Z = np.zeros_like(X)
+
+    # target = np.stack([X.flatten(), Y.flatten(), Z.flatten()], axis=-1)
+    target = np.stack([X, Y, Z], axis=-1)
+    source = pcd_rotation(target, 45, 0, 0)
+
     print(f"target: {target}")
 
     # pcd_show([target, coord])
@@ -86,12 +128,15 @@ if __name__ == "__main__":
     # ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap="viridis", color="b")
     # ax.plot_surface(X, Y, Z, cmap="viridis", color="b")
 
-    ax.scatter(X, Y, Z, color="b")
+    # ax.scatter(X, Y, Z, color="b")
+
+    ax.scatter(source[:, 0], source[:, 1], source[:, 2])
+    ax.grid(False)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
     ax.set_title("Points cloud on Surface")
-    # plt.show()
+    plt.show()
 
     source_nv = simple_normal_vector(source)
     print(f"source_nv: {source_nv}")
@@ -110,7 +155,7 @@ if __name__ == "__main__":
             normal[1],
             normal[2],
             length=0.2,
-            color="red",
+            color="purple",
         )
 
     plt.show()
--- layout/app_robot.py
+++ layout/app_robot.py
@@ -1,13 +1,17 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QMessageBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 import threading
 import time
 import os
-sys.path.append("D:\\Quan\\roboDK\\Vision-Machine-collab-Nhan\\VIKO_UltraRobot")
-from src import test_mulThread as T
-from src import robotic_modify as RM
+# sys.path.append("D:\\Quan\\roboDK\\Vision-Machine-collab-Nhan\\VIKO_UltraRobot")
+sys.path.append(
+    "E:\\Project\\Robot-6DOF\\VIKO_UltraRobot"
+)
+
+from src import motion_limit as ML
+# from src import robotic_modify as RM
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -59,15 +63,30 @@ class MainWindow(QMainWindow):
     def run_program(self):
         if self.program_running is True:
             print("Program is running...")
-            RM.mainRob()
-            # obj = T.TestThread("test")
-            # obj.run()
+            # RM.mainRob()
+            obj = ML.TestThread("test")
+            obj.run()
             time.sleep(1)
 
     def stop_program(self):
         self.program_running = False
         if self.program_thread and self.program_thread.is_alive():
             self.program_thread.join()
+        reply = QMessageBox()
+        reply.setText('Are you sure you want to exit the program?')
+        reply.setStandardButtons(QMessageBox.StandardButton.Yes | 
+                     QMessageBox.StandardButton.No)
+        
+        x = reply.exec()
+        
+        
+
+        if x == QMessageBox.standardButtons.Yes:
+            # Perform any necessary cleanup operations here
+            # ...
+
+            # Exit the Python program
+            sys.exit()
 
     def closeEvent(self, event):
         """Override the close event to stop the program thread before closing the window."""
