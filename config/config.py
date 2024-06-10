3a7cf85d38b36a95f90d620dfa648447a453dc6a
NTN-hacker <59677134+NTN-hacker@users.noreply.github.com>
2024-06-10 19:46:40 +0900

    Nhan: Develop CenterLine

---
+++ config/config.py
@@ -0,0 +1,57 @@
+"""
+config file
+"""
+
+# import torch
+
+# TODO
+
+
+proj = nn.Conv2d(
+            in_chans, embed_dim, kernel_size=kernel_size, stride=stride, padding=padding
+        )
+########################################################### ROBOTIC ###################################################################
+PIXEL_SIZE = 3.45
+LINEAR_SPEEDS = [50, 100]
+JOINT_SPEEDS = [10, 60]
+FOCAL_LENGTH = 16
+RESOLUTION_X = 2448  # DEFAULT RESOLUTION
+RESOLUTION_Y = 2048  # DEFAULT RESOLUTION
+HORIZONTAL_BASELINE = 55 # 65: best value with object is black dot
+VERTICAL_BASELINE = 450 # best value with object is black dot
+
+DISTANCE_LASERtoOBJECT = 120
+DISTANCE_CAMERA2OBJECT = 569
+
+
+########################################################## AI MODEL ###################################################################
+
+
+DEVICE = "cuda"  # if torch.cuda.is_available() else "cpu"
+MODEL = {
+    "SAM": {
+        "WEIGHT": "D:\\nhan\\viko\src\sam_vit_h_4b8939.pth",  # Path of your checkpoint
+        "MODEL_TYPE": "vit_h",
+    },
+    "LightWeight_SAM": {
+        "WEIGHT": "D:\\nhan\\viko\src\weight\mobile_sam.pt",  # Path of your checkpoint
+        "MODEL_TYPE": "vit_t",
+    },
+    "SEGMENT_WELD": {
+        "API_KEY": "JtRFLNmuxFdQiNLXfFJj",  # your api key
+        "PROJECT_NAME": "weld-detection-slz4d",
+        "CONFIDENT": 50,
+        "OVERLAP": 50,
+    },
+    "HQ_SAM": {
+        "WEIGHT": 'D:\\nhan\\viko\src\weight\sam_hq_vit_tiny.pth',
+        "MODEL_TYPE": "vit_tiny",
+    },
+    "YOLOV9": {
+        "WEIGHT": 'D:\\nhan\\viko\src\weight\\best.pt'
+    }
+}
+
+########################################################## COORDINATE #######################################################################
+X_RATIO = 2048/640 
+Y_RATIO = 2448/640
